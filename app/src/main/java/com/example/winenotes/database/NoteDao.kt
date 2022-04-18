e0155bc385c4e16ae8be2c064a9353b3602a1aa6
Will Stewart <wstewart6@my.apsu.edu>
2022-04-18 19:58:10 -0400

    added query functions to NoteDao

--- app/src/main/java/com/example/winenotes/database/NoteDao.kt
+++ app/src/main/java/com/example/winenotes/database/NoteDao.kt
@@ -1,8 +1,15 @@
 package com.example.winenotes.database
 
 import androidx.room.Dao
+import androidx.room.Query
 
 @Dao
 interface NoteDao {
+    // Gets all notes, sorted by title
+    @Query("SELECT * FROM note ORDER BY title")
+    fun getNotesByTitle() : List<Note>
 
+    // Gets all notes, sorted by date last modified
+    @Query("SELECT * FROM note ORDER BY lastModified")
+    fun getNotesByLastModified() : List<Note>
 }
