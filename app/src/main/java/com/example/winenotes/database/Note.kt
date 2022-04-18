efd797e56575cacb92f2296ebde7945ad18a0d6c
Will Stewart <wstewart6@my.apsu.edu>
2022-04-18 19:48:51 -0400

    setup Note class for note table

--- app/src/main/java/com/example/winenotes/database/Note.kt
+++ app/src/main/java/com/example/winenotes/database/Note.kt
@@ -1,5 +1,15 @@
 package com.example.winenotes.database
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
-class Note {
+@Entity
+class Note (
+    @PrimaryKey(autoGenerate = true) val id : Long,
+    @ColumnInfo val title : String,
+    @ColumnInfo val notes : String,
+    @ColumnInfo val lastModified : String
+) {
+
 }
