f993b80686050c31240a29915869190b2827c8fb
Will Stewart <wstewart6@my.apsu.edu>
2022-04-18 20:28:06 -0400

    added code for recyclerview

--- app/src/main/java/com/example/winenotes/MainActivity.kt
+++ app/src/main/java/com/example/winenotes/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.winenotes
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
+import android.view.*
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.winenotes.database.Note
 import com.example.winenotes.databinding.ActivityMainBinding
 
@@ -15,6 +18,46 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val layoutManager = LinearLayoutManager(this)
+
+    }
+
+    inner class MyViewHolder(val itemView: View) :
+        RecyclerView.ViewHolder(itemView), View.OnClickListener {
+
+//        init {
+//            itemView.findViewById<View>(R.layout.item_view)
+//                .setOnClickListener(this)
+//        }
+
+        fun setText(title: String, lastModified : String) {
+            itemView.findViewById<TextView>(R.id.note_title_textview).text = title
+            itemView.findViewById<TextView>(R.id.note_lastmodified_textview).append(" $lastModified")
+        }
+
+        override fun onClick(view: View?) {
+            if(view != null) {
+
+            }
+        }
+    }
+
+    inner class MyAdapter() : RecyclerView.Adapter<MyViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_view, parent, false)
+            return MyViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+            val note = NOTES[position]
+            holder.setText(note.title, note.lastModified)
+        }
+
+        override fun getItemCount(): Int {
+            return NOTES.size
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
