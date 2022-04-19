e334320ed8836d0e5d55c92633dd1b09de7ca1d6
Will Stewart <wstewart6@my.apsu.edu>
2022-04-18 21:17:17 -0400

    removed rounded corner drawable

--- app/src/main/java/com/example/winenotes/EditNoteActivity.kt
+++ app/src/main/java/com/example/winenotes/EditNoteActivity.kt
@@ -6,9 +6,15 @@ import com.example.winenotes.databinding.ActivityEditNoteBinding
 
 class EditNoteActivity : AppCompatActivity() {
     private lateinit var binding: ActivityEditNoteBinding
+    private var purpose : String? = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEditNoteBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val intent = getIntent()
+        purpose = intent.getStringExtra(getString(R.string.intent_purpose_key))
+
+        setTitle("$purpose")
     }
 }
