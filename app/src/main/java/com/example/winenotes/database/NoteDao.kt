48f474f175b6349ba453685820f179d59831d604
Will Stewart <wstewart6@my.apsu.edu>
2022-04-18 21:32:39 -0400

    added insert function to note dao

--- app/src/main/java/com/example/winenotes/database/NoteDao.kt
+++ app/src/main/java/com/example/winenotes/database/NoteDao.kt
@@ -1,10 +1,21 @@
 package com.example.winenotes.database
 
 import androidx.room.Dao
+import androidx.room.Insert
 import androidx.room.Query
 
 @Dao
 interface NoteDao {
+
+    /**
+     * @param note the note object to insert
+     *          id should be 0.
+     *
+     * @return ID of note object just inserted
+     */
+    @Insert
+    fun addNote(note : Note) : Long
+
     // Gets all notes, sorted by title
     @Query("SELECT * FROM note ORDER BY title")
     fun getNotesByTitle() : List<Note>
