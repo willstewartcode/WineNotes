f2fd6ec0df4d6e98eb64beae77517ff481542744
Will Stewart <wstewart6@my.apsu.edu>
2022-04-18 20:11:27 -0400

    setup main menu in kotlin & added functions for menu buttons

--- app/src/main/java/com/example/winenotes/MainActivity.kt
+++ app/src/main/java/com/example/winenotes/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.winenotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.example.winenotes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +12,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.sort_by_title_menuoption -> sortByTitle()
+            R.id.sort_by_last_modified_menuoption -> sortByLastModified()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun sortByTitle() {
+        // TODO: Implement sort by title function
+    }
+
+    private fun sortByLastModified() {
+        // TODO: Implement sort by last modified function
     }
 }
